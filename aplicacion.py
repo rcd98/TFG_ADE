21bbe1b0765aaf4689fc96ecada8589b5924237b
rcd98 <61578816+RaulCachoDominguez@users.noreply.github.com>
2021-04-04 22:38:35 +0200

    añadidas las estrategias complejas

--- aplicacion.py
+++ aplicacion.py
@@ -5,6 +5,7 @@ from python.calculoPrimasMetodos.calculoPrimaMetodoBinomial import PrimaMetodoBi
 from python.estrategias.basicas.estrategiasBasicas import EstrategiasBasicas
 from python.estrategias.representacionEstrategias import Representacion
 from python.estrategias.tendencia.spreadAlcista import SpreadAlcista
+from python.estrategias.tendencia.spreadBajista import SpreadBajista
 
 aplicacion = Flask(__name__)
 
@@ -21,6 +22,11 @@ def calcularEstrategias():
 def complejas():
     titulo = "ESTRATEGIAS COMPLEJAS"
     return render_template("./Estrategias/calcularEstrategiasComplejas.html", titulo = titulo)
+
+@aplicacion.route("/spreadBajista")
+def spreadBajista():
+    titulo = "SPREAD BAJISTA"
+    return render_template("./Estrategias/tendencia/spreadBajista.html", titulo=titulo)
 
 @aplicacion.route("/spreadAlcista")
 def spreadAlcista():
@@ -51,11 +57,15 @@ def representacionTendencia():
         prima1 = float(prima1)
         contratos1 = request.form['contratos1']
         contratos1 = float(contratos1)
+        if (request.form['spread'] == "bajista"):
+            titulo = "SPREAD BAJISTA"
+            estrategia = SpreadBajista(ejercicio0, prima0, ejercicio1, prima1, tipoN, contratos0, contratos1)
+        else:
+            titulo = "SPREAD ALCISTA"
+            estrategia = SpreadAlcista(ejercicio0, prima0,ejercicio1, prima1, tipoN, contratos0,contratos1)
 
-    estrategia = SpreadAlcista(ejercicio0, prima0,ejercicio1, prima1, tipoN, contratos0,contratos1)
     r = Representacion(estrategia.eje_x, estrategia.eje_y, estrategia.eje_0, estrategia.nombre)
-    return render_template("./Estrategias/tendencia/representacionSpreadAlcista.html", ruta=r.rutaImagen, ejercicio0=ejercicio0, prima0=prima0, ejercicio1= ejercicio1, prima1=prima1, tipo=tipo, contratos0=contratos0, contratos1=contratos1)
-
+    return render_template("./Estrategias/tendencia/representacionSpread.html", ruta=r.rutaImagen, ejercicio0=ejercicio0, prima0=prima0, ejercicio1= ejercicio1, prima1=prima1, tipo=tipo, contratos0=contratos0, contratos1=contratos1, titulo=titulo)
 
 @aplicacion.route("/representacionBasicas", methods=['POST'])
 def representacionBasicas():
